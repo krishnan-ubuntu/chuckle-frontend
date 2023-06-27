4d8011de838bd3df7299419727d1d0fb03d17923
Krishnan <ks@geedesk.com>
2023-06-27 20:21:31 +0530

    basic ui view

---
+++ chuckle.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+	<title>Twitter Clone</title>
+</head>
+<body>
+	<div class="container"><h1 align="center">Twitter Clone</h1>
+		<br><br>
+		<div class="row">
+			<div class="col-xs-3 col-xs-3 col-md-3"></div>
+		    <div class="col-xs-6 col-xs-6 col-md-6">
+		    	<textarea class="form-control"></textarea><br>
+		    	<p align="right"><button class="btn btn-sm btn-primary">Chuckle</button></p>
+		    	<hr>
+		    	<br>
+		    </div>
+		    <div class="col-xs-3 col-xs-3 col-md-3"></div>
+		</div>
+		<br>
+	</div>  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>  
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+</body>  
+</html> 
