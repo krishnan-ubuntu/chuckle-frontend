0a9fd075b41c873a254326423a185d9764c21c65
Krishnan <ks@geedesk.com>
2023-07-01 18:54:01 +0530

    Modified the grid of chuckle page

--- chuckle.php
+++ chuckle.php
@@ -2,22 +2,26 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0"  />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 	<title>Twitter Clone</title>
 </head>
 <body>
 	<div class="container"><h1 align="center">Twitter Clone</h1>
 		<br><br>
-		<div class="row">
-			<div class="col-xs-3 col-xs-3 col-md-3"></div>
-		    <div class="col-xs-6 col-xs-6 col-md-6">
-		    	<textarea class="form-control"></textarea><br>
+		<div class="row">			
+			<div class="col-xs-6 col-md-6">
+				I will write a description here
+			</div>
+		    <div class="col-xs-6 col-md-6">
+		    	<textarea class="form-control" placeholder="What is on your mind?"></textarea><br>
 		    	<p align="right"><button class="btn btn-sm btn-primary">Chuckle</button></p>
 		    	<hr>
 		    	<br>
+		    	<p>Chuckles will be displayed here.</p>
 		    </div>
-		    <div class="col-xs-3 col-xs-3 col-md-3"></div>
+		    <!-- <div class="col-xs-3 col-md-3"></div> -->
 		</div>
 		<br>
 	</div>  
